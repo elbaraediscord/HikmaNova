04a7ca2097320f8dabc4971118a1e8ec0c6f017d
EpicCat <elbaraet@gmail.com>
2025-10-27 23:45:19 +0300

    fdafad

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { vitePluginManusRuntime } from "vite-plugin-manus-runtime";
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime()];
 
 export default defineConfig({
-  base: '/hikmanova/',
+  base: '/',
   plugins,
   resolve: {
     alias: {
